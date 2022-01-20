1639e12d177ace5e6250747bc6e6fc2575d4faef
SoySorio <98106436+SoySorio@users.noreply.github.com>
2022-01-20 12:03:17 -0500

    Contiene identificación de caracteres y suma de opciones

--- entropia_contrasenna.py
+++ entropia_contrasenna.py
@@ -1,12 +1,40 @@
 def run():
-    minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    simbolos = ['¿', '?', '¡', '!', '-', '_', ',', '.', '/', "'", '"', '$', '#', '%', '(', ')', '°', '´', '*', '+', '{', '}', '[', ']', '^', '<', '>']
+    min = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    may = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    sim = ['¿', '?', '¡', '!', '-', '_', ',', '.', '/', "'", '"', '$', '#', '%', '(', ')', '°', '*', '+', '{', '}', '[', ']', '^', '<', '>']
 
-    longitud = len(minusculas) + len(mayusculas) + len(numeros) + len(simbolos)
+    opciones = 0
+    tmin = False
+    tmay = False
+    tnum = False
+    tsim = False
 
-    print(longitud)
+
+    contrasenna = input( 'Ingresa una contraseña ' )
+
+    for i in contrasenna: 
+        if i in min:
+            tmin = True
+        elif i in may:
+            tmay = True
+        elif i in num:
+            tnum = True
+        elif i in sim:
+            tsim = True
+        
+        if tmin and tmay and tnum and tsim: break
+
+    if tmin: opciones += len( min )
+    if tmay: opciones += len( may )
+    if tnum: opciones += len( num )
+    if taim: opciones += len( sim )
+
+    print(opciones)
+        
+
+
+"""    longitud = len(minusculas) + len(mayusculas) + len(numeros) + len(simbolos)""" 
 
 
 if __name__ == '__main__':
