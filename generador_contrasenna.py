74c9093cbb83aadc750aabd8970999854101937d
SoySorio <joseandresvicunacelis@gmail.com>
2022-01-23 17:00:22 -0500

    Crea def generador_contrasenna.py

--- generador_contrasenna.py
+++ generador_contrasenna.py
@@ -1,7 +1,23 @@
 import random
 import alfabeto
+
+
+def generar_contrasenna( numero ):
+    contrasenna = ''
+
+    for i in range( numero ):
+        contrasenna += random.choice( alfabeto.todos )
+    
+    return contrasenna
+          
+
 def run():
-    pass
+    cantidad = input( '¿Cuántos caracteres quieres en tu contraseña? ' )
+    cantidad = int( cantidad )
+
+    contrasenna = generar_contrasenna( cantidad )
+
+    print( 'Contraseña: ' + contrasenna )    
 
 
 if __name__ == '__main__':
